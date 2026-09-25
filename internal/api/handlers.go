1554c0ccdd5492c135041dfd0c12aec67619162f
Mohamed Saker <mohamedsaker233@gmail.com>
2026-09-25 03:48:55 +0300

    created new file users.go to separate the logic

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -3,28 +3,13 @@ package api
 import (
 	"net/http"
 	"os"
-	"time"
-
-	"github.com/google/uuid"
 
 	db "github.com/Saker233/go-image-processing/internal/database"
-	"github.com/Saker233/go-image-processing/internal/util"
 	"github.com/gin-gonic/gin"
 )
 
 type Server struct {
 	store *db.Store
-}
-
-type userRequest struct {
-	Username string `json:"username" binding:"required,alphanum"`
-	Password string `json:"password" binding:"required,min=6"`
-}
-
-type userResponse struct {
-	ID        uuid.UUID `json:"id"`
-	Username  string    `json:"username"`
-	CreatedAt time.Time `json:"created_at"`
 }
 
 func SetupServer(store *db.Store) {
@@ -44,40 +29,6 @@ func (s *Server) getHealth(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Healthy",
 	})
-}
-
-func (s *Server) register(c *gin.Context) {
-	var req userRequest
-
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
-
-	hashed, err := util.CreateHashed([]byte(req.Password))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, errorResponse(err))
-		return
-	}
-
-	args := db.CreateUserParams{
-		Username:     req.Username,
-		PasswordHash: string(hashed),
-	}
-
-	user, err := s.store.CreateUser(c, args)
-	if err != nil {
-		c.JSON(http.StatusForbidden, errorResponse(err))
-		return
-	}
-
-	rsp := userResponse{
-		ID:        user.ID,
-		Username:  user.Username,
-		CreatedAt: user.CreatedAt,
-	}
-
-	c.JSON(http.StatusCreated, rsp)
 }
 
 func errorResponse(err error) gin.H {
---
+++ internal/api/users.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"net/http"
+	"time"
+
+	db "github.com/Saker233/go-image-processing/internal/database"
+	"github.com/Saker233/go-image-processing/internal/util"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type userRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type userResponse struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func (s *Server) register(c *gin.Context) {
+	var req userRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashed, err := util.CreateHashed([]byte(req.Password))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	args := db.CreateUserParams{
+		Username:     req.Username,
+		PasswordHash: string(hashed),
+	}
+
+	user, err := s.store.CreateUser(c, args)
+	if err != nil {
+		c.JSON(http.StatusForbidden, errorResponse(err))
+		return
+	}
+
+	rsp := userResponse{
+		ID:        user.ID,
+		Username:  user.Username,
+		CreatedAt: user.CreatedAt,
+	}
+
+	c.JSON(http.StatusCreated, rsp)
+}
